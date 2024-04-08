de73a1908008efc6cd02741616eba572463cb0c0
Javicorporation <krchipullajavier@gmail.com>
2024-04-08 13:23:59 -0500

    comando AS

--- SQL_AS.sql
+++ SQL_AS.sql
@@ -1,2 +1,5 @@
+--has sirve para establecer un alias a una columna seleccionada para asi tener una mejor comprencion
 SELECT col1 AS col_nombre1 FROM tabla;
-SELECT
+
+-- en este caso tenemos una tabla llamada "usuario" de la cual vamos a cambiar el nombre de la columna "nombre" a "usuarios"
+SELECT nombre AS cliente FROM usuarios;
