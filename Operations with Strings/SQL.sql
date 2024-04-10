28aa9325d5a48d8125bd68bbdfdb9f9e01565423
Javicorporation <krchipullajavier@gmail.com>
2024-04-09 22:09:17 -0500

    unfinished

--- Operations with Strings/SQL.sql
+++ Operations with Strings/SQL.sql
@@ -1,9 +1,13 @@
 -------------------------------------------------------------------------------------|
+-- Ordenando todos los datos y la funcion
 --
 --
 --
 -- *********************************
 -- Ejercicio
+-- Se tiene una tabla usuarios con las columnas nombre, apellido, email y teléfono.
+-- Utiliza lo aprendido para seleccionar los 3 correos más largos de la tabla. 
+-- El resultado debe mostrar dos columnas: una con los emails y otra con sus largos respectivos.
 
 
 
