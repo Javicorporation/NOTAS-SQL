c310152076da2c3c64fc4add5e536d11b98f7606
Javicorporation <krchipullajavier@gmail.com>
2024-04-19 23:02:21 -0500

    ok

---
+++ Inserting records/SQL_Inserting2.sql
@@ -0,0 +1,60 @@
+-------------------------------------------------------------------------------------|
+-- Añadir un registro especificando columnas
+--  A la hora de insertar datos es posible mencionar específicamente las columnas que se van a insertar, en lugar de mencionar todos los valores en el orden en que se definen en la tabla.
+
+Veamos un ejemplo:
+
+Se tiene la tabla productos:
+--
+
+-- *****
+-- Ejercicio
+--
+--
+--
+--
+
+
+
+-------------------------------------------------------------------------------------|
+--
+--
+--
+
+-- *****
+-- Ejercicio
+--
+--
+--
+--
+
+
+
+-------------------------------------------------------------------------------------|
+--
+--
+--
+
+-- *****
+-- Ejercicio
+--
+--
+--
+--
+
+
+
+-------------------------------------------------------------------------------------|
+--
+--
+--
+
+-- *****
+-- Ejercicio
+--
+--
+--
+--
+
+
+
